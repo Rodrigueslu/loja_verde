a840cfa2cd1100d29e06d959b8d597f81d39e3a1
Fauns0 <luizhhuu@gmail.com>
2023-11-08 19:50:51 -0400

    dao/conexao

---
+++ application/dao/Conexao.php
@@ -0,0 +1,27 @@
+<?php
+    class Conexao{
+        private $dbName = "loja";
+        private $usuario = "root";
+        private $senha  = "sucesso";
+        private $host = "localhost";
+        // carrega a conexão realizada com o banco
+        private $conn;
+        public function __construct(){
+            $this->conn = new \mysqli($this->host,$this->usuario,$this->senha,$this->$dbName);
+        }
+
+        public function conectar(){
+            if($this->conn->connect_error){
+                die("A conexão falhou.".$this->conn->connect_error);
+            }
+            return $this->conn;
+        }
+        public function desconectar(){
+            $this->conn->close();
+        }
+
+
+    }
+
+
+?>
